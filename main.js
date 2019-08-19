fb17bb19328787bdf586024005acbe6c2a72872b
angvidal <angvidal@publicisgroupe.net>
2019-08-19 13:45:45 -0400

    made it so after the user submits a todo the input field is then cleared

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 let input = document.getElementById('input-value');
 let submitButton = document.getElementById('submit');
 let list = document.getElementById('list');
-let listItem = document.createElement('li');
 let errorAlert = document.getElementById('error-text');
 let todos = [];
 
 const blackBook = {
   submitToDos: submitButton.onclick = function(e) {
     e.preventDefault();
-    let inputVal = input.value
-    if (inputVal === ""){
+    if (input.value == ""){
       return errorAlert.innerText = "enter something to do"
     }else{
-      todos.push(inputVal);
-      todos.join("");
+      todos.push(input.value);
       for (let i = 0; i < todos.length; i++){
-          // list.append(`<li>${todos[i]}</li>`);
+        list.innerHTML = `<li>${todos.join("</li><li>")}</li>`;
       }
-      console.log(todos)
+      return input.value = "";
     }
-    return list.innerHTML = `<li>${todos.join("</li><li>")} </li>`;
   }
 }
