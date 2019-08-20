4b2abb195b927d03df9ff917c9755e9fb807858d
angvidal <angvidal@publicisgroupe.net>
2019-08-20 11:50:16 -0400

    made it so when the user deletes list items its only the li's that are removed

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 let input = document.getElementById('input-value');
 let submitButton = document.getElementById('submit');
 let list = document.getElementById('list');
-let listItem = document.getElementsByClassName('list-item');
+let listItems = document.getElementsByTagName('li')
 let errorAlert = document.getElementById('error-text');
 let trashCan = document.getElementById('trash');
 let todos = [];
+// console.log(listItem) /
+
 
 const blackBook = {
-  submitToDos: submitButton.onclick = function(e) {
-    e.preventDefault();
-    if (input.value == ""){
-      return errorAlert.innerText = "enter something to do"
-    }else{
-      todos.push(input.value);
-      for (let i = 0; i < todos.length; i++){
-        list.innerHTML = `<li class='list-item'>${todos.join("</li><li>")} <i class="fas fa-trash" id="trash"></i></li>`;
-      }
-      return input.value = "";
+    submitToDos: submitButton.onclick = function(e) {
+        e.preventDefault();
+        if (input.value == "") {
+            return errorAlert.innerText = "enter something to do"
+        } else {
+            todos.push(input.value);
+            for (let i = 0; i < todos.length; i++) {
+                list.innerHTML = `<li>${todos.join("</li><li>")}</li>`;
+            }
+            // clears the input after user enters todo
+            return input.value = "";
+        }
+    },
+    deleteItems: trashCan.onclick = function() {
+         // list = document.getElementById("top");
+        while (list.firstChild) {
+          list.removeChild(list.firstChild);
+        }
+        // return list.remove();
     }
-  },
-  deleteItem: trashCan.onclick = function(e) {
-     return listItem.remove();
-     // let whiteSpace =  document.createTextNode("");
-     // return listItem.appendChild(editInput)
-  }
 }
